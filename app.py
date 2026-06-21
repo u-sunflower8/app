b4557a86cac9b0033f4d2bf715b36f9a957c379d
u-sunflower8 <99594907+u-sunflower8@users.noreply.github.com>
2026-06-21 10:47:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("接続テスト")
 try:
     # 接続試行
     # (ここに必要な認証コードを記述)
-    sheet = client.open("Todoリスト").sheet1
+sheet = client.open_by_key("101hhNwt1VrR0fn3me63ifsduMqelnVIrY1ozr_Ul74w").sheet1
     st.write("成功！シートに接続できました。")
     st.write("セルA1の内容:", sheet.cell(1, 1).value) # 1行1列目の値を表示
 except Exception as e:
